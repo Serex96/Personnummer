39f92e1cabbfb14d02f8ce8d1a9e5cd8246872e6
Serex96 <christian.strandbrink@live.se>
2015-01-20 20:19:05 +0100

    Personnummer

---
+++ Personnummer.ps1
@@ -0,0 +1,7 @@
+#Detta script frÃ¥gar om ditt fÃ¶rnamn och efternamn samt vilket Ã¥r du Ã¤r fÃ¶dd sedan kommer ditt personnummer ut.#
+
+$firstname=read-host "Vad är ditt förnamn?"
+$efternamn=read-host "Vad är ditt efternamn?"
+$year=read-host "Vilket år är du född?"
+
+write-host $year
